3a894f4f3a88d97e8e826943188447e647afdc05
jaguardeer <pdeloz@gmail.com>
2023-01-11 10:35:56 -0600

    initial work on template matching

---
+++ count_stars.py
@@ -0,0 +1,11 @@
+import cv2
+
+SUBSTAT_STRINGS = {
+	"HP": "hp",
+	"ATK": "atk",
+	"DEF": "def",
+	"CRIT Rate": "critRate",
+	"CRIT DMG": "critDMG",
+	"Energy Recharge": "enerRech",
+	"Elemental Mastery": "eleMas",
+}
