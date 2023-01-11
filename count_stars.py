e4c0bbf74c5dc93333719a60902ee2dfec3379d2
jagdeer <pdeloz@gmail.com>
2023-01-11 12:26:33 -0600

    further exploring template matching: best fits

--- count_stars.py
+++ count_stars.py
@@ -1,17 +1,43 @@
 import cv2 as cv
 import numpy as np
-from matplotlib import pyplot as plt
+import time
 
+start = time.time()
+
+## todo: match on binarized version
 img_rgb = cv.imread('artifact-page.png')
 img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
-template = cv.imread('star-template.png', 0)
-mask = cv.imread('res2.png', 0)
+template = cv.imread('test-template.png', 0)
+#mask = cv.imread('res2.png', 0)
+
+loadTime = time.time()
 
 w, h = template.shape[::-1]
-res = cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED, mask = mask)
+res = cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED)
+
+matchTime = time.time()
+
+## multiple matching
 threshold = 0.97
 loc = np.where( res >= threshold)
 for pt in zip(*loc[::-1]):
-	cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+	cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + yyh), (0,0,255), 2)
+
+drawTime = time.time()
+
+
+## best fit matching
+minVal, maxVal, minLoc, maxLoc = cv.minMaxLoc(res)
+
+findTime = time.time()
+
+pt = maxLoc
+print(maxVal)
+cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (255,0,0), 2)
+
+
+## write results
 cv.imwrite('res.png',img_rgb)
 print(len(list(zip(*loc[::-1]))))
+
+end = time.time()
--- gen-font-image.py
+++ gen-font-image.py
@@ -1,30 +1,36 @@
 import numpy as np
 from PIL import ImageFont, ImageDraw, Image
-import cv2
+import cv2 as cv
 import time
 
 # note: font from https://www.dafontfree.io/genshin-impact-font/
+## load font
+fontpath = "./zh-cn.ttf"     
+font = ImageFont.truetype(fontpath, 32)
 
-## Make canvas and set the color
-## Colors are in BGR order
+## text config
+text = "Energy Recharge+4.7%"
+## colors are in BGR order
 grey = (92, 77, 71)
 cream = (215, 228, 233)
 bgColor_BGR = cream
-img = np.full((200,400,3),bgColor_BGR,np.uint8)
 fgColor_BGR = grey
 
-## Use cv2.FONT_HERSHEY_XXX to write English.
-text = time.strftime("%Y/%m/%d %H:%M:%S %Z", time.localtime()) 
-#cv2.putText(img,  text, (50,50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, fgColor_BGR, 1, cv2.LINE_AA)
-
-## Write from font file
-fontpath = "./genshin-font.ttf"     
-font = ImageFont.truetype(fontpath, 32)
+## create canvas
+box = font.getbbox(text)
+img = np.full((box[3], box[2], 3), bgColor_BGR, np.uint8)
 img_pil = Image.fromarray(img)
+
+## draw text
 draw = ImageDraw.Draw(img_pil)
-draw.text((5, 10),  "Energy Recharge+9.7%\nElemental Mastery+35\nDEF+5.1%\nCRIT DMG+13.2%", font = font, fill = fgColor_BGR)
+draw.text((0, 0), text, font = font, fill = fgColor_BGR)
 img = np.array(img_pil)
 
-## Display 
-cv2.imshow("res", img);cv2.waitKey();cv2.destroyAllWindows()
-cv2.imwrite("res.png", img)
+## (WIP) resize to fit game text
+scale = 19.5/25
+img = cv.resize(img, None, fx = scale, fy = scale)
+
+## display and write file
+imgName = "test-template"
+cv.imshow(imgName, img);cv.waitKey();cv.destroyAllWindows()
+cv.imwrite(f"{imgName}.png", img)
