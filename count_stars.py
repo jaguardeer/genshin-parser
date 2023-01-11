f701a8e8d6dfa1f9de7f4e54181fd856f1bc3d36
jaguardeer <pdeloz@gmail.com>
2023-01-11 17:39:56 -0600

    add ROI selection

--- count_stars.py
+++ count_stars.py
@@ -1,43 +1,40 @@
 import cv2 as cv
 import numpy as np
 import time
-
-start = time.time()
+from util import *
 
 ## todo: match on binarized version
-img_rgb = cv.imread('artifact-page.png')
-img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
+sourceImg = cv.imread('artifact-page.png')
+grayImg = cv.cvtColor(sourceImg, cv.COLOR_BGR2GRAY)
 template = cv.imread('test-template.png', 0)
 #mask = cv.imread('res2.png', 0)
 
-loadTime = time.time()
+## select ROI
+roi = cv.selectROI(sourceImg)
+cropImg = crop(grayImg, roi)
+show(cropImg)
 
-w, h = template.shape[::-1]
-res = cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED)
+h, w, *channels = template.shape
+## todo: learn what different matching methods do
+matchImg = cv.matchTemplate(cropImg, template, cv.TM_CCOEFF_NORMED)
 
-matchTime = time.time()
-
+"""
 ## multiple matching
 threshold = 0.97
 loc = np.where( res >= threshold)
 for pt in zip(*loc[::-1]):
-	cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + yyh), (0,0,255), 2)
-
-drawTime = time.time()
-
+	cv.rectangle(sourceImg, pt, (pt[0] + w, pt[1] + yyh), (0,0,255), 2)
+"""
 
 ## best fit matching
-minVal, maxVal, minLoc, maxLoc = cv.minMaxLoc(res)
-
-findTime = time.time()
+minVal, maxVal, minLoc, maxLoc = cv.minMaxLoc(matchImg)
 
 pt = maxLoc
 print(maxVal)
-cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (255,0,0), 2)
+cv.rectangle(cropImg, pt, (pt[0] + w, pt[1] + h), (255,0,0), 2)
 
 
-## write results
-cv.imwrite('res.png',img_rgb)
-print(len(list(zip(*loc[::-1]))))
-
-end = time.time()
+## show, write results
+show(cropImg)
+cv.imwrite('res.png', cropImg)
+# print(len(list(zip(*loc[::-1]))))
