c227e141aa8fa6cee60bf4856ce249735158509f
jaguardeer <pdeloz@gmail.com>
2023-01-11 17:33:01 -0600

    binarize text image; move funcs to util.py

--- gen-font-image.py
+++ gen-font-image.py
@@ -2,19 +2,21 @@ import numpy as np
 from PIL import ImageFont, ImageDraw, Image
 import cv2 as cv
 import time
+from util import *
 
 # note: font from https://www.dafontfree.io/genshin-impact-font/
 ## load font
-fontpath = "./zh-cn.ttf"     
-font = ImageFont.truetype(fontpath, 32)
+fontPath = "./zh-cn.ttf"     
+fontSize = 32
+font = ImageFont.truetype(fontPath, fontSize)
 
 ## text config
 text = "Energy Recharge+4.7%"
 ## colors are in BGR order
-grey = (92, 77, 71)
+gray = (92, 77, 71)
 cream = (215, 228, 233)
 bgColor_BGR = cream
-fgColor_BGR = grey
+fgColor_BGR = gray
 
 ## create canvas
 box = font.getbbox(text)
@@ -27,8 +29,19 @@ draw.text((0, 0), text, font = font, fill = fgColor_BGR)
 img = np.array(img_pil)
 
 ## (WIP) resize to fit game text
-scale = 19.5/25
-img = cv.resize(img, None, fx = scale, fy = scale)
+#scale = 19.5/25
+#img = cv.resize(img, None)
+
+## gen binarized image
+grayImg = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+_, binImg = cv.threshold(grayImg, 128 ,255, cv.THRESH_BINARY_INV)
+show(grayImg)
+show(binImg)
+cv.imshow("test", binImg);cv.waitKey();cv.destroyAllWindows()
+
+## find bounding box
+boundBox = cv.boundingRect(binImg)
+cv.rectangle(img, boundBox, (0, 0, 255))
 
 ## display and write file
 imgName = "test-template"
