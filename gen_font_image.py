c227c28c523685fc62a7436c35d915fce4b6c955
jaguardeer <pdeloz@gmail.com>
2023-01-11 21:21:44 -0600

    split into funcs

--- gen_font_image.py
+++ gen_font_image.py
@@ -4,46 +4,46 @@ import cv2 as cv
 import time
 from util import *
 
-# note: font from https://www.dafontfree.io/genshin-impact-font/
-## load font
-fontPath = "./zh-cn.ttf"     
-fontSize = 32
-font = ImageFont.truetype(fontPath, fontSize)
+def main():
+	# note: font from https://www.dafontfree.io/genshin-impact-font/
+	## load font
+	fontPath = "./zh-cn.ttf"     
+	fontSize = 32
+	font = ImageFont.truetype(fontPath, fontSize)
+	## colors are in BGR order
+	gray = (92, 77, 71)
+	cream = (215, 228, 233)
+	bgColor = cream
+	fgColor = gray
 
-## text config
-text = "Energy Recharge+4.7%"
-## colors are in BGR order
-gray = (92, 77, 71)
-cream = (215, 228, 233)
-bgColor_BGR = cream
-fgColor_BGR = gray
+	img = createTemplate("Energy Recharge+4.7%", font, fgColor, bgColor)
+	## display and write file
+	show(img)
+	imgName = "test-template"
+	cv.imwrite(f"{imgName}.png", img)
 
-## create canvas
-box = font.getbbox(text)
-img = np.full((box[3], box[2], 3), bgColor_BGR, np.uint8)
-img_pil = Image.fromarray(img)
+def drawText(text, font, fgColor, bgColor):
+	## create canvas
+	box = font.getbbox(text)
+	img = np.full((box[3], box[2], 3), bgColor, np.uint8)
+	img_pil = Image.fromarray(img)
+	## draw text
+	draw = ImageDraw.Draw(img_pil)
+	draw.text((0, 0), text, font = font, fill = fgColor)
+	img = np.array(img_pil)
+	return img
 
-## draw text
-draw = ImageDraw.Draw(img_pil)
-draw.text((0, 0), text, font = font, fill = fgColor_BGR)
-img = np.array(img_pil)
+## returns a template for use in template_match.py
+def createTemplate(text, font, fgColor, bgColor):
+	## gen binarized image
+	textImg = drawText(text, font, fgColor, bgColor)
+	grayImg = cv.cvtColor(textImg, cv.COLOR_RGB2GRAY)
+	_, binImg = cv.threshold(grayImg, 128 ,255, cv.THRESH_BINARY_INV)
 
-## (WIP) resize to fit game text
-scale = 19.5/25
-img = cv.resize(img, None, fx = scale, fy = scale)
+	## find bounding box
+	boundBox = cv.boundingRect(binImg)
+	binImg = crop(binImg, cv.boundingRect(binImg))
 
-## gen binarized image
-grayImg = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
-_, binImg = cv.threshold(grayImg, 128 ,255, cv.THRESH_BINARY_INV)
-show(grayImg)
-show(binImg)
+	return binImg
 
-## find bounding box
-boundBox = cv.boundingRect(binImg)
-cv.rectangle(img, boundBox, (0, 0, 255))
-binImg = crop(binImg, cv.boundingRect(binImg))
-
-## display and write file
-imgName = "test-template"
-cv.imshow(imgName, img);cv.waitKey();cv.destroyAllWindows()
-cv.imwrite(f"{imgName}.png", binImg)
+if __name__ == "__main__": main()
