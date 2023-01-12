968e4759b7fded630f247a036fc7818ad0390d93
jaguardeer <pdeloz@gmail.com>
2023-01-11 21:52:54 -0600

    split matching func from driver

--- template_match.py
+++ template_match.py
@@ -3,48 +3,41 @@ import numpy as np
 import time
 from util import *
 
-sourceImg = cv.imread('artifact-page.png')
-template = cv.imread('test-template.png', 0)
-#mask = cv.imread('res2.png', 0)
+def getMatchCoeff(img, template):
+	## resize to match template size
+	h, w, *channels = template.shape
+	resized = cv.resize(img, (w, h))
+	## todo: learn what different matching methods do
+	matchImg = cv.matchTemplate(resized, template, cv.TM_CCOEFF_NORMED)
+	## best fit matching
+	minVal, maxVal, minLoc, maxLoc = cv.minMaxLoc(matchImg)
+	print(f"match coeff is {round(maxVal, 3)}")
+	return maxVal, maxLoc
 
-## select ROI
-roi = cv.selectROI(sourceImg)
-cropImg = crop(sourceImg, roi)
-grayImg = cv.cvtColor(cropImg, cv.COLOR_BGR2GRAY)
-_, binImg = cv.threshold(grayImg, 128 ,255, cv.THRESH_BINARY_INV)
-binImg = crop(binImg, cv.boundingRect(binImg))
-binImg = cv.resize(binImg, template.shape[::-1])
+def main():
+	sourceImg = cv.imread('artifact-page.png')
+	template = cv.imread('test-template.png', 0)
 
-# debug stuff
-show(binImg)
-boundBox = cv.boundingRect(binImg)
-cv.rectangle(cropImg, boundBox, (0, 0, 255))
-show(cropImg)
+	## select ROI
+	roi = cv.selectROI(sourceImg)
+	cropImg = crop(sourceImg, roi)
 
-h, w, *channels = template.shape
+	## binarize and crop to bounding box
+	grayImg = cv.cvtColor(cropImg, cv.COLOR_BGR2GRAY)
+	_, binImg = cv.threshold(grayImg, 128 ,255, cv.THRESH_BINARY_INV)
+	binRect = cv.boundingRect(binImg)
+	binImg = crop(binImg, binRect)
 
-## todo: learn what different matching methods do
-matchImg = cv.matchTemplate(binImg, template, cv.TM_CCOEFF_NORMED)
+	## run template matching
+	maxVal, maxLoc = getMatchCoeff(binImg, template)
 
-"""
-## multiple matching
-threshold = 0.97
-loc = np.where( res >= threshold)
-for pt in zip(*loc[::-1]):
-	cv.rectangle(sourceImg, pt, (pt[0] + w, pt[1] + yyh), (0,0,255), 2)
-"""
+	## draw rect around matched point
+	pt = (maxLoc[0] + roi[0], maxLoc[1] + roi[1])
+	w, h = binRect[2] - binRect[0], binRect[3] - binRect[1]
+	cv.rectangle(sourceImg, pt, (pt[0] + w, pt[1] + h), (255,0,0), 2)
 
-## best fit matching
-minVal, maxVal, minLoc, maxLoc = cv.minMaxLoc(matchImg)
-print(f"match coeff is {round(maxVal, 3)}")
+	## show, write results
+	show(sourceImg)
+	cv.imwrite('res.png', cropImg)
 
-pt = (maxLoc[0] + roi[0], maxLoc[1] + roi[1])
-cv.rectangle(sourceImg, pt, (pt[0] + w, pt[1] + h), (255,0,0), 2)
-
-## show, write results
-show(sourceImg)
-cv.imwrite('res.png', cropImg)
-
-
-## binarize, make bounding box, scale to template size. can use:
-#cv.resize(img, targetSize)
+if __name__ == "__main__": main()
