e80711dd34040330679ff46f8c7d5bd19a7740d3
jaguardeer <pdeloz@gmail.com>
2023-12-21 15:02:05 -0600

    auto-update name-set-slot lookup table from wiki

--- updateGOODspec.py
+++ updateGOODspec.py
@@ -13,25 +13,24 @@ def readURL(url):
 		return None
 
 
-
+# get artifact set wiki page
 artifact_wiki_url = "https://genshin-impact.fandom.com/wiki/Artifact/Sets"
 html = readURL(artifact_wiki_url)
 print(f"Reading wiki at {artifact_wiki_url}")
 if html == None:
 	print(f"Couldn't read URL: {artifact_wiki_url}")
 	exit()
+artifact_set_page = BeautifulSoup(html, features="html.parser")
 
 
+# get artifact set table from page
+table = artifact_set_page.table # artifact table is (probably) first table on page
+# validate table
 def sanitize(string):
 	def is_allowed(c):
 		allowed = '_'
 		return c.isalnum() or c in allowed
 	return ''.join(c.lower() for c in string if is_allowed(c))
-
-bs = BeautifulSoup(readURL("https://genshin-impact.fandom.com/wiki/Artifact/Sets"), features="html.parser")
-
-table = bs.table # artifact table is (probably) first table on page
-# validate table
 header = [sanitize(i.get_text()) for i in table.find_all('th')]
 expected_header = ['set', 'rarity', 'pieces', 'bonuses']
 if header != expected_header:
@@ -39,9 +38,19 @@ if header != expected_header:
 	exit()
 
 
-data = [row.find_all('td') for row in table.find_all('tr') if row.td != None]
+# parse table
+table_rows = [row.find_all('td') for row in table.find_all('tr') if row.td != None]
 
+from strKeyDicts import SET_KEY_STRINGS, NAME_SET_SLOT_KEY_STRINGS
 
-for i in data:
-	name, rarity, pieces, bonuses = (x.get_text() for x in i)
-	print(f'Name: {i[0]}')
+def to_pascal(name):
+	return ''.join(c for c in ''.join(word.capitalize()
+		for word in name.split(' '))
+		if c.isalnum())
+
+slotKeys = ['flower', 'plume', 'sands', 'goblet', 'circlet']
+item2setslot = {
+	piece: {'setKey': to_pascal(name.get_text()), 'slotKey': slotKey}
+	for name, rarity, pieces, bonuses in table_rows
+	for piece, slotKey in zip({to_pascal(p.get('alt')) for p in pieces.find_all('img')}, slotKeys)
+}
